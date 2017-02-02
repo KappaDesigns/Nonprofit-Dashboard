37f32aa7e72410d566cf02cecfe4a74cac0d9c02
ecoulson <ivansonofcoul@gmail.com>
2017-02-01 22:54:09 -0800

    completed html page test with image and text editing backend also provided theory for page logic (#7)

--- tests/pages.js
+++ tests/pages.js
@@ -1,47 +1,55 @@
 const redis = require('redis');
 const client = redis.createClient();
 
-// Theory
-/*
-    Create a hashmap in redis which is page:id this stores the html class or ids
-    of text and image elems
-
-    ________
-    | Spec |
-    --------
-
-    - id of page would be the page name in nav/url. Consider storing in custom
-    data-attribute on the body of each page?
-
-    - html ids would probably be better option due to the uniqueness. Consider
-      custom data-attribute on text/img elems for ease of access
-
-    - Store with string. String should be prefixed by the node type e.g [TXT]: or
-      [IMG]: in order to parse where values go front end
-
-        Jquery Psuedo
-        key -> key from redis hashmap
-        value -> get from key
-        prefix -> value split : index 0
-        data -> value split : index 1
-
-        if (prefix === [TXT])
-          $(key) text = data
-        else
-          $(key) src = data
-
-    ---------------------------------------------------------------------------
-      Testing
-    ---------------------------------------------------------------------------
-
-    To garuntee results create mock html in console that is a struct that is
-    either txt or img, and contains some data field.
-
-     This can be initialized in start
-
-*/
-
-
 client.on('error', (err) => {
   console.log(`Error: ${err}`);
+});
+
+let type = {
+  TXT: 'TXT',
+  IMG: 'IMG'
+}
+
+const page = "home";
+let html = {
+
+}
+
+function addElement(name, attribute, data, type) {
+  client.hmset(`page:${name}`, attribute, `[${type.toUpperCase()}]:${data}`, (err, res) => {
+    if (err) {
+      console.log(err);
+      process.exit();
+    }
+  })
+}
+
+function getPage(name, next) {
+  client.hgetall(`page:${name}`, (err, res) => {
+    if (err) {
+      return next(err)
+    }
+    next(null, res);
+  })
+}
+
+function setHtml(data) {
+  for (let key in data) {
+    let split = data[key].split(':');
+    let prefix = split[0];
+    let value = split[1];
+    html[key] = {
+      'type': prefix,
+      'value': value
+    }
+  }
+}
+
+getPage(page, (err, data) => {
+  if (err) {
+    console.log(err);
+    process.exit();
+  }
+  setHtml(data);
+  console.log(html);
 });
