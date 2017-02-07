99f61b9a59c6cf04efb563ac34304846f39f65b6
ecoulson <ivansonofcoul@gmail.com>
2017-02-06 18:24:51 -0800

    User redis backend updated so that user can be found by getting username. Passport installed

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const session = require('express-session');
 
 //routes
 const user = require('./routes/users');
@@ -13,6 +16,9 @@ const carousel = require('./routes/carousel');
 const events = require('./routes/events');
 const editor = require('./routes/editor');
 
+//data
+const User = require('./routes/data/User');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -20,6 +26,42 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  (username, password, next) => {
+    User.getUsername(username, (err, user) => {
+      console.log(username);
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    })
+  }
+))
+
+passport.serializeUser((user, next) => {
+  next(null, user.id);
+})
+
+passport.deserializeUser((id, next) => {
+  User.get(id, (err, data) => {
+    next(null, user)
+  })
+})
 
 app.use('/api', user);
 app.use('/api', news);
--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,48 @@ const router = express.Router();
 const User = require('./data/User');
 
 router.get('/user/:id', (req, res, next) => {
-  User.get(req.params.id, (data) => {
+  User.get(req.params.id, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(data);
+  })
+})
+
+router.get('/login/:username', (req, res, next) => {
+  User.getUsername(req.params.username, (err, data) => {
+    if (err) {
+      return next(err);
+    }
     res.send(data);
   })
 })
 
 router.post('/user', (req, res, next) => {
   User.getID((id) => {
-    User.create(id, req.body, (data) => {
-      res.send(data);
+    User.create(id, req.body, (err, data) => {
+      if (err) {
+        return next(err);
+      }
+      res.json(data);
     })
   })
 })
 
 router.put('/user/:id', (req, res, next) => {
-  User.put(req.params.id, req.body, (data) => {
+  User.put(req.params.id, req.body, (err, data) => {
+    if (err) {
+      return next(err);
+    }
     res.send(data);
   })
 })
 
 router.delete('/user/:id', (req, res, next) => {
-  User.delete(req.params.id, (data) => {
+  User.delete(req.params.id, (err, data) => {
+    if (err) {
+      return next(err);
+    }
     res.send(data)
   });
 })
