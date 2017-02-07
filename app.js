1da5de6a98f03eff386b6cb6cf03eb8eca463d5b
ecoulson <ivansonofcoul@gmail.com>
2017-02-06 19:58:25 -0800

    Passport seralizes and logs in user. However user not saved to session

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const client = require('redis').createClient();
 
 //routes
 const user = require('./routes/users');
@@ -23,10 +24,13 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(logger('dev'));
-app.use(cookieParser());
+app.use(cookieParser('keyboard cat'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
+  store: new RedisStore({
+    'client': client
+  }),
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
@@ -34,24 +38,6 @@ app.use(session({
 }))
 app.use(passport.initialize());
 app.use(passport.session());
-
-passport.use(new LocalStrategy(
-  (username, password, next) => {
-    User.getUsername(username, (err, user) => {
-      console.log(username);
-      if (err) {
-        return next(err);
-      }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (user.password !== password) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    })
-  }
-))
 
 passport.serializeUser((user, next) => {
   next(null, user.id);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const User = require('./data/User');
 
+passport.use(new LocalStrategy(
+  (username, password, next) => {
+    User.getUsername(username, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return next(null, false, { message: 'Incorrect username.' });
+      }
+      if (user.password !== password) {
+        return next(null, false, { message: 'Incorrect password.' });
+      }
+      return next(null, user);
+    })
+  }
+));
+
 router.get('/user/:id', (req, res, next) => {
+  console.log(req);
   User.get(req.params.id, (err, data) => {
     if (err) {
       return next(err);
@@ -11,13 +31,25 @@ router.get('/user/:id', (req, res, next) => {
   })
 })
 
-router.get('/login/:username', (req, res, next) => {
-  User.getUsername(req.params.username, (err, data) => {
+router.post('/login/', (req, res, next) => {
+  passport.authenticate('local', {session: true}, (err, user, info) => {
     if (err) {
       return next(err);
     }
-    res.send(data);
-  })
+    if (!user) {
+      return res.status(404).send({ message: 'Failed To Login', success: false });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      console.log(req);
+      return res.status(200).send({
+        message:'Success',
+        success: true,
+      });
+    })
+  })(req,res,next)
 })
 
 router.post('/user', (req, res, next) => {
