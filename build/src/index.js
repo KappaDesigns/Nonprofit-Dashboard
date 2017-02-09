858525f07c8452168f07edd88e43b9df40bfbbab
unholydeath785 <ivansonofcoul@gmail.com>
2017-02-08 18:30:09 -0800

    set up css and sass compiliation, also react router imported

--- build/src/index.js
+++ build/src/index.js
@@ -1,1 +1,14 @@
-console.log("compiled");
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hasHistory }
+import "./styles/css/main.css";
+
+class App extends React.Component {
+	render() {
+		return (
+			<
+		)
+	}
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ data/user.js
@@ -0,0 +1,27 @@
+const redis = require("redis");
+const client = redis.createClient();
+
+class User {
+	constructor(uID, data) {
+		console.log(uID);
+		this.id = uID;
+		for (let key in data) {
+			this.add(data[key]);
+		}
+	}
+
+	add(data) {
+		client.hmset(`user:${this.uID}`, data, (err, res) => {
+			if (err) {
+				return console.log(err);
+			}
+			console.log(`{user:${this.uID}}: ${res}`);
+		})
+	}
+
+	get() {
+		client.hgetall(`user:${this.uID}`, (err, res) => {})
+	}
+}
+
+module.exports = User;
