f9923d1a3745901d9c834305a25524526dc95c25
ecoulson <ivansonofcoul@gmail.com>
2017-02-09 23:48:36 -0800

    completed 404 with slightly updated image

--- build/src/scenes/NotFound/index.js
+++ build/src/scenes/NotFound/index.js
@@ -6,10 +6,10 @@ export default class NotFound extends React.Component {
     return (
       <div id="not-found">
         <div className="jumbotron">
-
+          <h1 className="notfound">404: Not Found</h1>
         </div>
         <h1 className="not-found-text">Looks like the page you were looking for doesn't exist!</h1>
-        <Link to="/" className="home">Get Me Out!</Link>
+        <Link to="/" className="home">Take Me Home!</Link>
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
       {
         test: /\.css$/,
         loader: "style-loader!css-loader",
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+        loader: 'url-loader?importLoaders=1&limit=100000'
       }
     ]
   },
