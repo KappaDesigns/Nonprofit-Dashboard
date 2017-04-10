891812626a5c2e3e958b156675ee5a0732b8ece6
ecoulson <ivansonofcoul@gmail.com>
2017-04-09 23:16:40 -0700

    carousel edit display working. Need masonry display of all carousels below. Also need to deal with updating the redis db

--- build/src/scenes/Carousel/EditPanel/index.js
+++ build/src/scenes/Carousel/EditPanel/index.js
@@ -5,84 +5,120 @@ export default class Carousel extends React.Component {
     super();
     this.state = {
       currentSlideIndex: 0,
+      currentCarousel: [],
       currentSlide: {},
     }
+    this.handleNext = this.handleNext.bind(this);
+    this.handlePrev = this.handlePrev.bind(this);
+    this.handleActive = this.handleActive.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
   componentWillMount() {
     let slide = this.props.carousel[0];
     this.setState({
-      carousel: this.props.carousel,
+      currentCarousel: this.props.carousel,
       currentSlide: slide
     })
   }
 
   render() {
-    let i = 0;
+    let index = 0;
+    let self = this;
     let style = {
       backgroundImage: `url(${this.state.currentSlide.src})`,
       backgroundSize: `cover`,
+      backgroundRepeat: `no-repeat`,
     }
+
+    let createSlide = (slide, index) => {
+      let style = {
+        backgroundSize: `cover`,
+        backgroundRepeat: `no-repeat`,
+        backgroundImage: `url(${slide.src})`
+      }
+      if (index - 1 == self.state.currentSlideIndex) {
+        return (
+          <div onClick={this.handleActive} id={`slide-${index}`} key={index} draggable style={style} className="draggable-slide active"></div>
+        )
+      } else {
+        return (
+          <div onClick={this.handleActive} id={`slide-${index}`} key={index} draggable style={style} className="draggable-slide"></div>
+        )
+      }
+    }
+
     return (
       <div className="carousel-container">
         <h1 className="header">Edit Carousel</h1>
         <div style={style} className="carousel-display">
           <div className="desc-container">
-            <textarea className="desc" type="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </textarea>
+            <textarea onChange={this.handleEdit} value={this.state.currentSlide.desc} className="desc" type="text"></textarea>
           </div>
-          <span className="remove-slide">Delete Slide</span>
+          <span onClick={this.handleDelete} className="remove-slide">Delete Slide</span>
         </div>
 
         <div className="carousel-controls">
-          <span className="prev">{"<"}</span>
-          <span className="next">{">"}</span>
+          <span onClick={this.handlePrev} className="prev">{"<"}</span>
+          <span onClick={this.handleNext} className="next">{">"}</span>
         </div>
 
         <div className="carousel-order">
-          {
-            this.props.carousel.map((slide) => {
-              i++;
-              let style = {
-                backgroundSize: `cover`,
-                backgroundRepeat: `no-repeat`,
-                backgroundImage: `url(${slide.src})`
-              }
-              if (i == 1) {
-                return (
-                  <div key={i} draggable style={style} className="draggable-slide active"></div>
-                )
-              } else {
-                return (
-                  <div key={i} draggable style={style} className="draggable-slide"></div>
-                )
-              }
-            })
-          }
+          {this.props.carousel.map(createSlide, 0)}
         </div>
       </div>
     )
   }
 
   handleNext() {
-
+    let index = this.state.currentSlideIndex;
+    if (index < this.state.currentCarousel.length - 1) {
+      index++;
+    } else {
+      index = 0;
+    }
+    this.setState({
+      currentSlideIndex: index,
+      currentSlide: this.state.currentCarousel[index]
+    })
   }
 
   handlePrev() {
-
+    let index = this.state.currentSlideIndex;
+    if (index > 0) {
+      index--;
+    } else {
+      index = this.state.currentCarousel.length - 1;
+    }
+    this.setState({
+      currentSlideIndex: index,
+      currentSlide: this.state.currentCarousel[index]
+    })
   }
 
-  handleActive() {
-    
+  handleActive(e) {
+    let index = e.target.id.split("-")[1] - 1;
+    this.setState({
+      currentSlideIndex: index,
+      currentSlide: this.state.currentCarousel[index]
+    })
   }
 
-  handleEdit() {
-
+  handleEdit(e) {
+    let slide = this.state.currentSlide;
+    slide.desc = e.target.value
+    this.setState({
+      currentSlide: slide
+    })
   }
 
   handleDelete() {
-
+    let index = this.state.currentSlideIndex;
+    this.state.currentCarousel.splice(index, 1);
+    this.setState({
+      currentSlide: this.state.currentCarousel[index],
+    })
   }
 
   handleAdd() {
--- build/src/scenes/Carousel/index.js
+++ build/src/scenes/Carousel/index.js
@@ -7,7 +7,7 @@ export default class Carousel extends React.Component {
   render() {
     return (
       <div>
-        <EditPanel carousel={[{src:"https://static.pexels.com/photos/3247/nature-forest-industry-rails.jpg"}, {src:"http://www.projecthappyhearts.com/wp-content/uploads/2015/04/green-nature-dual-monitor-other.jpg"}]}></EditPanel>
+        <EditPanel carousel={[{src:"https://static.pexels.com/photos/3247/nature-forest-industry-rails.jpg", desc:"desc"}, {src:"http://www.projecthappyhearts.com/wp-content/uploads/2015/04/green-nature-dual-monitor-other.jpg", desc:"desc"}, {src:"https://www-tc.pbs.org/wnet/nature/files/2016/09/mezzanine_329.jpg", desc:"desc"}]}></EditPanel>                                           
         <CarouselList></CarouselList>
       </div>
     )
